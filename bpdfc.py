b5098e7e045fdaba09740714d97f1dc4d8ffa47c
svictorino <sergio@sergiovictorino.me>
2023-03-07 18:16:44 -0300

    Deployment changed to Job so I wouldn't have to use infinite loops Removed loop from python code

--- bpdfc.py
+++ bpdfc.py
@@ -5,20 +5,17 @@ import time
 # assign directory
 directory = 'PDFs'
 
-while True:
-    time.sleep(6)
-    print("Procurando arquivos...") 
-
-    for filename in os.listdir(directory):
-        f = os.path.join(directory, filename)
-        # checking if it is a file
-        if os.path.isfile(f):
-            print(f) 
-            fcompr = (f.replace(".pdf" , "_compr.pdf" , 3))
-            if os.path.isfile(fcompr):
-                print ("O arquivo já existe, abortando: " + fcompr)
-                break
-            else:
-                print ("Convertendo arquivo: " + f)
-                print ("Destino: " + fcompr)
-                compress.compress(f, fcompr, power=4)
+print("Procurando arquivos...") 
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        print(f) 
+        fcompr = (f.replace(".pdf" , "_compr.pdf" , 3))
+        if os.path.isfile(fcompr):
+            print ("O arquivo já existe, abortando: " + fcompr)
+            break
+        else:
+            print ("Convertendo arquivo: " + f)
+            print ("Destino: " + fcompr)
+            compress.compress(f, fcompr, power=4)
